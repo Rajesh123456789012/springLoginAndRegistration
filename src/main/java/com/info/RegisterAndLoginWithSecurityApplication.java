a5253b3b93ff6103d888fbfc05d40002826e96e7
Rajesh <rajeshhelpss@gmail.com>
2025-12-06 11:17:46 +0530

    Intial commit

---
+++ src/main/java/com/info/RegisterAndLoginWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAndLoginWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAndLoginWithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/info/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.info.config;
+
+import com.info.service.CustomUserDetails;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	private final CustomUserDetails customUserDetails;
+
+	public SecurityConfig(CustomUserDetails customUserDetails) {
+		this.customUserDetails = customUserDetails;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(request -> request.requestMatchers("/register", "/login").permitAll()
+						.anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/welcome", true).permitAll())
+				.logout(logout -> logout.logoutSuccessUrl("/login").permitAll()).userDetailsService(customUserDetails);
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
